f68adbe1265884369b0077f15e99a0ed4cf2485d
Simun Strukan <simun.strukan@sintezis.co>
2019-12-05 12:32:41 +0100

    Added Elude project to dashboard

--- js/projects.js
+++ js/projects.js
@@ -3,11 +3,11 @@ var projects =
 	"sintezis":
 	{
 		"slack": {
-			"url": "https://sintezis.sintezis.slack",
+			"url": "https://slack.sintezis.slack",
 			"infoUrl": "http://wiki.sintezis.co/books/public-wiki/page/management#bkmrk-slack"
 		},
 		"kanban": {
-			"url": "http://kanban.sinezis.co",
+			"url": "http://kanban.sintezis.co",
 			"infoUrl": "http://wiki.sintezis.co/books/public-wiki/page/management#bkmrk-restyaboard"
 		},
 		"zoom": {
@@ -54,6 +54,53 @@ var projects =
 
 	"elude": 
 	{
-
+		"slack": {
+			"url": "https://eludetodayllc.slack.com",
+			"infoUrl": "http://wiki.sintezis.co/books/public-wiki/page/management#bkmrk-slack"
+		},
+		"kanban": {
+			"url": "http://kanban.sintezis.co/#/organization/3",
+			"infoUrl": "http://wiki.sintezis.co/books/public-wiki/page/management#bkmrk-restyaboard"
+		},
+		"zoom": {
+			"url": "https://zoom.us",
+			"infoUrl": ""
+		},
+		"wiki": {
+			"url": "http://wiki.sintezis.co/books/elude",
+			"infoUrl": "http://wiki.sintezis.co/books/public-wiki/page/management#bkmrk-bookstack"
+		},
+		"cloud": {
+			"url": "https://cloud.sintezis.co/index.php/s/mtfDqgAGEqrtpqx",
+			"infoUrl": "http://wiki.sintezis.co/books/public-wiki/page/management#bkmrk-nextcloud"
+		},
+		"doc": {
+			"url": "http://docusign.com/home",
+			"infoUrl": "http://wiki.sintezis.co/books/public-wiki/page/management#bkmrk-docusign"
+		},
+		"drawio": {
+			"url": "http://draw.io",
+			"infoUrl": "http://wiki.sintezis.co/books/public-wiki/page/management#bkmrk-drawio"
+		},
+		"swagger": {
+			"url": "http://api.elude.sintezis.co/api/documentation",
+			"infoUrl": ""
+		},
+		"figma": {
+			"url": "https://www.figma.com/file/URmFivzVx22wnjQulAqijj/Elude-Mobile-Design?node-id=2%3A13819",
+			"infoUrl": "http://wiki.sintezis.co/books/public-wiki/page/management#bkmrk-figma"
+		},
+		"gitlab": {
+			"url": "http://gitlab.sintezis.co/sintezis/elude",
+			"infoUrl": ""
+		},
+		"proxmox": {
+			"url": "https://195.201.197.31:8006",
+			"infoUrl": ""
+		},
+		"firebase": {
+			"url": "https://console.firebase.google.com/u/0/project/elude-dev/overview",
+			"infoUrl": ""
+		}
 	}
 }
